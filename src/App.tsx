1d0316fc09157943323f6e42e54169a9ef3ad858
Eva Archer <evaarch@udel.edu>
2026-08-27 11:10:27 -0400

    most of task 3 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,10 +8,27 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <header className="new-header"> I am adding a new Header </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Eva Archer was here! Hello World
             </p>
+
+            <img src="../src/DSC_0654.JPG" alt="A picture of my dog Tango!" />
+            <p> My dog is : </p>
+            <ul>
+                <li>amazing</li>
+                <li>lazy</li>
+                <li>cute</li>
+                <li>lowkey evil</li>
+            </ul>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
         </div>
     );
 }
