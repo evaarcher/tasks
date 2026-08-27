4003ce029b97e0c2ea890ca18d2be87f180fb976
Eva Archer <evaarch@udel.edu>
2026-08-26 20:45:59 -0400

    made npm run check

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CIqSC275 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Eva Archer was here!
+                automatically reload. Eva Archer was here! Hello World
             </p>
         </div>
     );
