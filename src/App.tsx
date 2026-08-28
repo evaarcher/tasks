f5c2727d58a5bf94c99920181740c8d214932424
Eva Archer <evaarch@udel.edu>
2026-08-28 12:09:44 -0400

    finished rectange and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <header className="new-header"> I am adding a new Header </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Eva Archer was here! Hello World
-            </p>
+        <>
+            <div className="App">
+                <header className="App-header">
+                    UD CISC275 with React Hooks and TypeScript
+                    <h2>this is a new header!</h2>
+                </header>
+                <p>
+                    Edit <code>src/App.tsx</code> and save. This page will
+                    automatically reload. Eva Archer was here! Hello World
+                </p>
 
-            <img src="../src/DSC_0654.JPG" alt="A picture of my dog Tango!" />
-            <p> My dog is : </p>
-            <ul>
-                <li>amazing</li>
-                <li>lazy</li>
-                <li>cute</li>
-                <li>lowkey evil</li>
-            </ul>
-            <Button
-                onClick={() => {
-                    console.log("Hello World!");
-                }}
-            >
-                Log Hello World
-            </Button>
-        </div>
+                <img src="dog.JPG" alt="A picture of my dog Tango!" />
+                <p> My dog is : </p>
+                <ul>
+                    <li>amazing</li>
+                    <li>lazy</li>
+                    <li>cute</li>
+                    <li>lowkey evil</li>
+                </ul>
+                <Button
+                    onClick={() => {
+                        console.log("Hello World!");
+                    }}
+                >
+                    Log Hello World
+                </Button>
+            </div>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            {" "}
+                            <span
+                                style={{
+                                    display: "inline-block",
+                                    width: "100px",
+                                    height: "50px",
+                                    backgroundColor: "red",
+                                }}
+                            />
+                        </Col>
+                        <Col>
+                            {" "}
+                            <span
+                                style={{
+                                    display: "inline-block",
+                                    width: "100px",
+                                    height: "50px",
+                                    backgroundColor: "red",
+                                }}
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </>
     );
 }
 
