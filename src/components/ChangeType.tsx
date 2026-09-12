e321d2dbc2579d54f7d6398d523870228f3ef12d
eva archer <evaarch@udel.edu>
2026-09-12 11:44:55 -0400

    counter, change type, start attempt and change answer done

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,24 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [question, setQuestion] = useState<QuestionType>(
+        "short_answer_question",
+    );
+    function changeQuestion(): void {
+        setQuestion(
+            question === "multiple_choice_question" ?
+                "short_answer_question"
+            :   "multiple_choice_question",
+        );
+    }
+    return (
+        <div>
+            <Button onClick={changeQuestion}>Change Type</Button>
+            <div>
+                {question === "multiple_choice_question" ?
+                    <span>Multiple Choice</span>
+                :   <span>Short Answer</span>}
+            </div>
+        </div>
+    );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,39 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempt, setAttempt] = useState<number>(4);
+    const [started, setStarted] = useState<boolean>(false);
+    function startQuiz(): void {
+        setAttempt(attempt - 1);
+        setStarted(true);
+    }
+    function stopQuiz(): void {
+        setStarted(false);
+    }
+    function increaseAttempts(): void {
+        setAttempt(attempt + 1);
+    }
+    return (
+        <div>
+            <div>
+                <Button onClick={startQuiz} disabled={attempt === 0 || started}>
+                    Start Quiz
+                </Button>
+            </div>
+            <div>
+                <Button onClick={stopQuiz} disabled={!started}>
+                    Stop Quiz
+                </Button>
+            </div>
+            <div>
+                <Button
+                    onClick={increaseAttempts}
+                    disabled={started ? true : false}
+                >
+                    Mulligan
+                </Button>
+            </div>
+            <div>Attempts left : {attempt} </div>
+        </div>
+    );
 }
