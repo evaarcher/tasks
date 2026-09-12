0cce0d1e400e70b8cd119c3e89efd0f52052584f
eva archer <evaarch@udel.edu>
2026-09-12 12:55:19 -0400

    finished all things :)

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,41 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-
+export type Holiday = "🎄" | "🎂" | "🎃" | "🎆" | "🌎";
+//christmas, my Birthday(sept 2), halloween, new years DAY , earth day
+const ALPHABETICAL_TRANSITIONS: Record<Holiday, Holiday> = {
+    "🎂": "🎄",
+    "🎄": "🌎",
+    "🌎": "🎃",
+    "🎃": "🎆",
+    "🎆": "🎂",
+};
+const NUMERICAL_TRANSITION: Record<Holiday, Holiday> = {
+    "🎆": "🌎",
+    "🌎": "🎂",
+    "🎂": "🎃",
+    "🎃": "🎄",
+    "🎄": "🎆",
+};
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [hoilday, setHoliday] = useState<Holiday>("🎂");
+    function alphChange(): void {
+        const newHoliday = ALPHABETICAL_TRANSITIONS[hoilday];
+        setHoliday(newHoliday);
+    }
+    function numChange(): void {
+        const newHoliday = NUMERICAL_TRANSITION[hoilday];
+        setHoliday(newHoliday);
+    }
+    return (
+        <div>
+            Cycle Holiday
+            <div>Holiday: {hoilday}</div>
+            <div>
+                <Button onClick={alphChange}>Advance by Alphabet</Button>
+            </div>
+            <div>
+                <Button onClick={numChange}>Advance by Year</Button>
+            </div>
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,39 @@ export function d6(): number {
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeftDie] = useState<number>(1);
+    const [rightDie, setRightDie] = useState<number>(2);
+    function rollLeft(): void {
+        setLeftDie(d6());
+    }
+    function rollRight(): void {
+        setRightDie(d6());
+    }
+    function rateRoll(left: number, right: number): string {
+        return (
+            left === right ?
+                left === 1 ?
+                    "You Lose!"
+                :   "You Win!"
+            :   ""
+        );
+    }
+    return (
+        <div>
+            Two Dice
+            <div>
+                <span data-testid="left-die">Left Die :{leftDie}</span>
+            </div>
+            <div>
+                <span data-testid="right-die">Right Die :{rightDie}</span>
+            </div>
+            <div>
+                <Button onClick={rollLeft}>Roll Left</Button>
+            </div>
+            <div>
+                <Button onClick={rollRight}>Roll Right</Button>
+            </div>
+            <span>{rateRoll(leftDie, rightDie)}</span>
+        </div>
+    );
 }
